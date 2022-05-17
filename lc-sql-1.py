9a902ab10ed24ff3c1bdfffbd7ea9031888d85f5
Yoshino <377905356@qq.com>
2022-05-18 01:18:23 +0800

    lcsql_1

--- lc-sql-1.py
+++ lc-sql-1.py
@@ -22,4 +22,30 @@
 #
 # 按 任意顺序 返回结果表。
 #
-# 查询结果格式如下例所示。
+# 查询结果格式如下例所示。示例：
+#
+# 输入：
+# World 表：
+# +-------------+-----------+---------+------------+--------------+
+# | name        | continent | area    | population | gdp          |
+# +-------------+-----------+---------+------------+--------------+
+# | Afghanistan | Asia      | 652230  | 25500100   | 20343000000  |
+# | Albania     | Europe    | 28748   | 2831741    | 12960000000  |
+# | Algeria     | Africa    | 2381741 | 37100000   | 188681000000 |
+# | Andorra     | Europe    | 468     | 78115      | 3712000000   |
+# | Angola      | Africa    | 1246700 | 20609294   | 100990000000 |
+# +-------------+-----------+---------+------------+--------------+
+# 输出：
+# +-------------+------------+---------+
+# | name        | population | area    |
+# +-------------+------------+---------+
+# | Afghanistan | 25500100   | 652230  |
+# | Algeria     | 37100000   | 2381741 |
+# +-------------+------------+---------+
+#
+SELECT name,population,area FROM world WHERE area >= 3000000 OR population >= 25000000
+
+#2
+SELECT name,population,area FROM world where area >= 3000000
+UNION
+SELECT name,population,area FROM world where population >= 25000000
